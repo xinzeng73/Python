0769515a55eaadf626e635b8331040a54a5dd26e
Alex Xinzeng Wei <32911167+xinzeng73@users.noreply.github.com>
2021-11-05 12:26:03 -0700

    two sum use dictionary, can use either sorted or not sorted list

---
+++ two_sum.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/two-sum/
+
+numbers = [2,7,11,15]
+target = 9
+
+numbers = [2,7,11,15]
+target = 9
+
+def twoSum(nums, target):
+    dic={}
+    for i,j in enumerate(nums):
+        if target - j in dic:
+            return dic[target-j], i
+        else:
+            dic[j]=i
+
+print(twoSum(numbers, target))
+
